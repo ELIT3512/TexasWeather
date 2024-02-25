69eae5abcfbde5a1d831e39f91a396aac76fe4a8
-h <kevinezzel92@gmail.com>
2024-02-25 16:28:20 -0600

    locationAPIinfo

--- location.js
+++ location.js
@@ -1,18 +1,43 @@
 require("dotenv").config();
 const fetch = require("node-fetch")
-const weahterKey = process.env.weahterKey;
-console.log("weatherKey: ",weahterKey);
+const weatherKey = process.env.WeatherKey;
+console.log("weatherKey: ",weatherKey);
 
     async function texasApi(){
-        let url =`htts & query = Texas`;
+
+        let url =`http://api.weatherstack.com/current?access_key=${weatherKey}&query=Austin,Texas`;
         try{
             const res = await fetch(url);
             const data = await res.json();
-            console.log("data",data);
+            let dataLocation = data.location;
+            let dataCurrent = data.current;
+            let currentTemp = dataCurrent.temperature;
+            let localTime = dataLocation.localtime;
+            let city = dataLocation.name;
+            let state = dataLocation.region;
+            let descriptions = dataCurrent.weather_descriptions;
+            let splitTime = localTime.split(" ");
+            let date = splitTime[0];
+            let time = splitTime[1];
+            console.log(splitTime)
+            let localTimeE = document.getElementById("LoactionTime");
+            let dateE = document.getElementById("LocationDate");
+            let cityE = document.getElementById("city")
+            let stateE = document.getElementById("state");
+            let descritionE = document.getElementById("description");
+            let tempE = document.getElementById("temp");
+
+ 
+            localTimeE.textContent = time;
+            dateE.textContent = date;
         }catch(err){
         console.log(err);
         }
-        console.log("data",data);
+        // document.addEventListener("DOMContentLoaded",(event)=>{
+        
+        // })
+        
+        
     }
 
 texasApi()
